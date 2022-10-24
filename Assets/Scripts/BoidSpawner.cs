ef400896082586295c76bca80f08ee2cebc2be3d
Savro01 <romain.savaete@outlook.com>
2022-10-24 16:16:36 +0200

    Import des arbres pour l'environnement

--- Assets/Scripts/BoidSpawner.cs
+++ Assets/Scripts/BoidSpawner.cs
@@ -12,7 +12,7 @@ public class BoidSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 100; i++)
         {
             genZombie();
         }
